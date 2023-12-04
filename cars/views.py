57785e22e0bcdceca6dc116282a888fd0083226e
Willamy Domingos <willamy.wlp@gmail.com>
2023-12-04 20:55:20 -0300

    Implement login_required cars

--- cars/views.py
+++ cars/views.py
@@ -3,14 +3,9 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-
-class NewCarCreateView(CreateView):
-    model = Car
-    form_class = CarModelForm
-    template_name = 'new_car.html'
-    success_url = '/cars_list/'
 
 
 class CarsListView(ListView):
@@ -27,10 +22,21 @@ class CarsListView(ListView):
 
         return cars
 
+
 class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
 
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars_list/'
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -43,6 +49,7 @@ class CarUpdateView(UpdateView):
         )
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     def get(self, request, pk):
         object = get_object_or_404(Car, pk=pk)
