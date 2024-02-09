3a438c6d320e7d91168f8504a0a469a51d1304f0
Willamy Domingos <willamy.wlp@gmail.com>
2024-02-09 14:40:39 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = ['static',]
+STATICFILES_DIRS = ['static',]
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 # Default primary key field type
